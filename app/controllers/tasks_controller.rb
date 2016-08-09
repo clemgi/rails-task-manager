c8dc80d8b7c29f260a396a91d7235883906dedeb
clemence gillion <clemencegillion@gmail.com>
2016-08-09 12:57:30 +0200

    exercice

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,7 +33,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name)
+    params.require(:task).permit(:name, :content)
   end
 
    def set_task
